0c4abe4ad9b88a0ed79f061fd34691cc5fcf42a0
paybee <dev@payb.ee>
2014-04-21 23:57:30 +0200

    difficulty retarget changes

--- backends/bitcoind/blockchain_processor.py
+++ backends/bitcoind/blockchain_processor.py
@@ -187,8 +187,8 @@ class BlockchainProcessor(Processor):
 
     def read_chunk(self, index):
         with open(self.headers_filename, 'rb') as f:
-            f.seek(index*240*80)
-            chunk = f.read(240*80)
+            f.seek(index*1*80)
+            chunk = f.read(1*80)
         return chunk.encode('hex')
 
     def write_header(self, header, sync=True):
@@ -200,7 +200,7 @@ class BlockchainProcessor(Processor):
             self.flush_headers()
 
         with self.cache_lock:
-            chunk_index = header.get('block_height')/240
+            chunk_index = header.get('block_height')/1
             if self.chunk_cache.get(chunk_index):
                 self.chunk_cache.pop(chunk_index)
 
